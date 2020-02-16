ea2f33860037bf30eec9cdf68c093265625b8b0b
meijiishinislame <meijiishinislame@gmail.com>
2020-02-15 23:35:06 -1000

    testing

--- modules/process_data.py
+++ modules/process_data.py
@@ -4,21 +4,21 @@ import os
 
 import modules.arrivals as arrivals
 
-# stopnum =  "999"
-# arrival_id = 1509831815
-# route_number =  13
-# direction = "Eastbound"
-# estimated = 1
-# canceled = "0"
-# arrival_estimated = "2020-02-09 22:08:00"
-# arrival_scheduled = "2020-02-09 22:04:00"
+stopnum =  "999"
+arrival_id = 1509831815
+route_number =  13
+direction = "Eastbound"
+estimated = 1
+canceled = "0"
+arrival_estimated = "2020-02-09 22:08:00"
+arrival_scheduled = "2020-02-09 22:04:00"
 
-# arrival_dict = {}
-# arrival_dict["1"] = arrivals.Arrival(stopnum, arrival_id, route_number, direction, canceled, estimated, arrival_estimated, arrival_scheduled)
+arrival_dict = {}
+arrival_dict["1"] = arrivals.Arrival(stopnum, arrival_id, route_number, direction, canceled, estimated, arrival_estimated, arrival_scheduled)
 
-# arrival_dict["1"].arrived = True
-# arrival_dict["1"].minutes_off = 4
-# arrival_dict["1"].added = False
+arrival_dict["1"].arrived = True
+arrival_dict["1"].minutes_off = 4
+arrival_dict["1"].added = False
 
 def update_arrivals_db(arrival_dict):
 	conn = psycopg2.connect(database=str(os.environ["DB_NAME"]),
@@ -52,4 +52,4 @@ def update_arrivals_db(arrival_dict):
 		 	conn.commit()
 	conn.close()
 
-#update_arrivals_db(arrival_dict)
+update_arrivals_db(arrival_dict)
